dbe647837176ca43ae7ed755817e2187db28db39
Vivek <84849703+vt57299@users.noreply.github.com>
2023-03-16 17:25:47 +0530

    Update question1.py

--- question1.py
+++ question1.py
@@ -2,41 +2,60 @@
 
 class Node:
     def __init__(self,data):
-        self.data = data
+        self.data= data
         self.ref = None
-
-class LinkedList:
+    
+class linkedlist:
     def __init__(self):
         self.head = None
+
+    def add(self,data):                          
+        new_node = Node(data)
+        new_node.ref = self.head                 # Changing the refrence or link with the previous head
+        self.head = new_node
     
     def display(self):
         if self.head is None:
-            print("LinkedList is empty!!!")
+            print("Linkedlist is empty")
         else:
             n = self.head
             while n is not None:
                 print(n.data)
                 n = n.ref
     
-    def insert(self,data):
-        new_node = Node(data)
-        new_node.ref = self.head
-        self.head = new_node
+    def remove_zero_sum(self):
+        dummy = Node(0)
+        dummy.ref = self.head
+        prefix_sum = 0
+        seen = {0: dummy}
+        current = self.head
 
-    def delete(self):
-        if self.head is None:
-            print("linkedlist is empty so, can't delete the element!!!")
-        else:
-            self.head = self.head.ref
-    def delete_sum_zero(self):
-        if self.head is None:
-            print("linkedlist is empty so, can't delete the element!!!")
-        else:
-            self
+        while current:
+            prefix_sum += current.data
+            seen[prefix_sum] = current
+            current = current.ref
 
-obj= LinkedList()
-obj.insert(10)
-obj.insert(20)
-obj.insert(30)
-obj.delete()
+        prefix_sum = 0
+        current = dummy
+
+        while current:
+            prefix_sum += current.data
+            if prefix_sum in seen:
+                current.ref = seen[prefix_sum].ref
+            current = current.ref
+
+        self.head = dummy.ref
+
+obj = linkedlist()
+obj.add(10)
+obj.add(20)
+obj.add(-30)
+obj.add(30)
+obj.add(20)
+obj.add(10)
+print("Original linked list")
+obj.display()
+
+print("\nFinal linked list")
+obj.remove_zero_sum()
 obj.display()
